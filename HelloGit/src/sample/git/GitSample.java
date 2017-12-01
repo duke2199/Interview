dd5c81f7fdf04a521460eca56d1a8f4d08a64108
Du <duke2199@gmail.com>
2017-12-01 02:34:28 -0500

    testing

--- HelloGit/src/sample/git/GitSample.java
+++ HelloGit/src/sample/git/GitSample.java
@@ -5,6 +5,8 @@ public class GitSample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github");
+		
+		System.out.println("testing");
 	}
 
 }
